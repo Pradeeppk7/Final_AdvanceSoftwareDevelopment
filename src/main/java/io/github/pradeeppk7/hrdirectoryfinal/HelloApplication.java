3275dd8f4317ebf59024128abbf028518bf64aa0
Pradeeppk7 <pradeepkumar.pk002@gmail.com>
2026-04-25 12:28:10 -0500

    Getting started

---
+++ src/main/java/io/github/pradeeppk7/hrdirectoryfinal/HelloApplication.java
@@ -0,0 +1,9 @@
+package io.github.pradeeppk7.hrdirectoryfinal;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
